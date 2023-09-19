015c63b9a861fb01d5fea22732df212920f21b02
ghostflyby <ghostflyby+git@outlook.com>
2023-09-19 15:04:59 +0800

    Suppress NonJavaMixin

--- src/main/kotlin/com/ghostflyby/textbind/mixins/IGuiChat.kt
+++ src/main/kotlin/com/ghostflyby/textbind/mixins/IGuiChat.kt
@@ -6,6 +6,7 @@ import org.spongepowered.asm.mixin.Mixin
 import org.spongepowered.asm.mixin.gen.Accessor
 
 @Mixin(GuiChat::class)
+@Suppress("NonJavaMixin")
 interface IGuiChat {
   @get:Accessor val inputField: GuiTextField
 }
